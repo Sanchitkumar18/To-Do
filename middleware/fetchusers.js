0ab518b692f385fe0826cb5d5fe063c6293740fb
Sanchit Kumar <hcpssanchit@gmail.com>
2025-03-03 02:51:20 +0530

    minor fixes

--- middleware/fetchusers.js
+++ middleware/fetchusers.js
@@ -1,17 +1,22 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
-const jwt_secret = "dance123";
-const fetchuser=(req,res,next)=>{
-    //Get the user from the jwt token and add id to the req object
-    const token=req.header('auth-token');
-    if(!token){
-        res.status(401).send({error:"please authenticate using the valid token"})
+const jwt_secret = process.env.JWT_SECRET; 
+
+const fetchuser = (req, res, next) => {
+    // Get the user from the JWT token and add ID to the req object
+    const token = req.header('auth-token');
+    if (!token) {
+        return res.status(401).send({ error: "Please authenticate using a valid token" });
     }
+
     try {
-        const data=jwt.verify(token, jwt_secret);
-        req.user=data.user;
+        const data = jwt.verify(token, jwt_secret);
+        req.user = data.user;
         next();
     } catch (error) {
-        res.status(401).send({error:"please authenticate using the valid token"})
+        return res.status(401).send({ error: "Please authenticate using a valid token" });
     }
-}
-module.exports=fetchuser;
+};
+
+module.exports = fetchuser;
